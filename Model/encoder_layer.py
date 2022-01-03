89d65f2df431d4f5bb75795f19cb8184e159c1da
Derek Li <40778597+bayerndd@users.noreply.github.com>
2022-01-03 21:30:49 +0000

    Update encoder_layer.py

--- Model/encoder_layer.py
+++ Model/encoder_layer.py
@@ -1,13 +1,14 @@
-import torch.nn as nn
 from Model.pos_feed_forward import PositionwiseFeedForward
 from Model.multi_head_attn import MultiHeadAttention
+import Model.param as param
+import torch.nn as nn
 
 
 class EncoderLayer(nn.Module):
     def __init__(self):
         super(EncoderLayer, self).__init__()
-        self.enc_attn = MultiHeadAttention()
-        self.pff = PositionwiseFeedForward()
+        self.enc_attn = MultiHeadAttention(param.embed_dim, param.num_heads)
+        self.pff = PositionwiseFeedForward(param.embed_dim, param.d_ff)
 
     def forward(self, enc_input, enc_attn_mask):
         """
